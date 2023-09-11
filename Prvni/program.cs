0a34b051ba83d930904a32af5c179fffa5b141c9
radek-spilka <radek@spilkovi.cz>
2023-09-11 15:00:54 +0200

    ahojda

--- Prvni/program.cs
+++ Prvni/program.cs
@@ -3,5 +3,17 @@ using System;
 class Program {
     public static void Main(string[] args) {
         Console.WriteLine("Ahoj");
+
+        Rectangle rect = new Rectangle();
+        rect.Draw(5, 10);
+    }
+}
+
+class Rectangle(float height, float width) {
+    float height;
+    float width;
+
+    public void Draw(){
+        Console.WriteLine("Drawing rectangle: size" + height + " " + width);
     }
 }
