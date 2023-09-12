1c88b11f92bde65aca2b9b6991bd291800396cf1
radek-spilka <radek@spilkovi.cz>
2023-09-12 17:31:25 +0200

    a

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Calculator
+{
+    public class Program
+    {
+        public static void Main (string[] args)
+        {
+
+        }
+    }
+
+    class Operator {
+        private float a;
+        private float b;
+
+        public Operator (float a, float b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        public float Sum(){
+            return a+b;
+        }
+
+        public float odecist(){
+            return a-b;
+        }
+
+        public float vynasobit(){
+            return a*b;
+        }
+
+        public float vydelit(){
+            try {
+                return a/b;
+            } catch{
+                return 0;
+            }
+        }
+    }
+}
