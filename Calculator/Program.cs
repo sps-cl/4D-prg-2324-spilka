aa5efae3ef204078cc565170c361d2c6eabe7569
radek-spilka <radek@spilkovi.cz>
2023-09-15 15:41:50 +0200

    Ja to zvladl ejkej ej mam rad jahody. Snad budu v prezentaci lol

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,42 +1,58 @@
-﻿using System;
-
-namespace Calculator
+﻿namespace Calculator
 {
     public class Program
     {
         public static void Main (string[] args)
         {
-
+            Console.WriteLine("Prvni cislo: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Druhe cislo: ");
+            int y = int.Parse(Console.ReadLine());
+            
+            CalculatorContext calc = new CalculatorContext(new OpAdd());
+            Console.WriteLine("OpAdd: "+calc.Execute(x, y));
+            calc.SetOperator(new OpSub());
+            Console.WriteLine("OpSub: "+calc.Execute(x, y));
+            calc.SetOperator(new OpMagic());
+            Console.WriteLine("OpMagic: "+calc.Execute(x, y));
         }
     }
 
-    class Operator {
-        private float a;
-        private float b;
+    public interface IOperator {
+        double Execute (int a, int b);
+    }
 
-        public Operator (float a, float b) {
-            this.a = a;
-            this.b = b;
-        }
-
-        public float Sum(){
+    public class OpAdd : IOperator {
+        public double Execute (int a, int b){
             return a+b;
         }
+    }
 
-        public float odecist(){
+    public class OpSub : IOperator {
+        public double Execute (int a, int b){
             return a-b;
         }
+    }
 
-        public float vynasobit(){
+    public class OpMagic : IOperator {
+        public double Execute (int a, int b){
             return a*b;
         }
+    }
 
-        public float vydelit(){
-            try {
-                return a/b;
-            } catch{
-                return 0;
-            }
+    public class CalculatorContext {
+        private IOperator ioperator;
+        
+        public CalculatorContext(IOperator ioperator) {
+            this.ioperator = ioperator;
+        }
+
+        public void SetOperator(IOperator ioperator) {
+            this.ioperator = ioperator;
+        }
+
+        public double Execute(int a, int b) {
+            return ioperator.Execute(a, b);
         }
     }
 }
