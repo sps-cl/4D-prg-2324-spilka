6a2f98e5ac5488d4fbd225d3dc62ec192fda80c5
radek-spilka <radek@spilkovi.cz>
2023-09-18 15:10:02 +0200

    kruci

---
+++ Library/Library/models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.models;
+
+namespace Library.models
+{
+    internal class Book : LibraryItem<Book>
+    {
+        public int NumberOfPages;
+        public string Author;
+        
+        public Book(int NumberOfPages, string Author, int id, string title) : base(id, title)
+        {
+            this.NumberOfPages = NumberOfPages;
+            this.Author = Author;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("Author: " + Author + ". NumberOfPager: " + NumberOfPages);
+        }
+    }
+}
